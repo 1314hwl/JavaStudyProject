f4ac295828faadf7cb1e6d21d028a1eb216055f1
henry <1314hwl@163.com>
2016-04-12 10:09:46 +0800

    Java project study
    progress every day!

---
+++ src/com/cn/sz/Test.java
@@ -0,0 +1,15 @@
+package com.cn.sz;
+/**
+ * 测试类
+ * @author HuWeiLiang
+ *
+ */
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hello world");
+
+	}
+
+}
